8fa7c08be3cc4d0acf185dd52842511343170852
conma13 <conma13@hotmail.com>
2026-02-12 02:17:41 +0100

    Move shared options to rootCommand

---
+++ dotnet/md2book/Commands/BuildPdf.cs
@@ -0,0 +1,31 @@
+﻿using md2book.Models;
+using md2book.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace md2book.Commands
+{
+    public class BuildPdf : Command
+    {
+        public BuildPdf(BuildPipeline pipeline) 
+            : base("pdf", "Create pdf file")
+        {
+            this.SetAction((ParseResult parseResult) =>
+            {
+                var ctx = new BuildContext
+                {
+                    InputFolder = parseResult.GetValue<string>("--input"),
+                    OutputFile = parseResult.GetValue<string>("--output"),
+                    Title = parseResult.GetValue<string>("--title"),
+                    TocLevel = parseResult.GetValue<int>("--toclevel")
+                };
+
+                pipeline.Run(ctx);
+            });
+
+        }
+    }
+}
--- dotnet/md2book/Program.cs
+++ dotnet/md2book/Program.cs
@@ -1,26 +1,81 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.CommandLine;
 using md2book.Commands;
-//using md2book.Pipeline;
-//using md2book.Pipeline.Steps;
-//using md2book.Services;
+using md2book.Pipeline;
+// using md2book.Pipeline.Steps;
+// using md2book.Services;
 
-var services = new ServiceCollection();
+namespace md2book
+{
+    public static class Program
+    {
+        static int Main(string[] args)
+        {
+            var services = new ServiceCollection();
 
-// Services
-//services.AddSingleton<IMarkdownParser, MarkdownParser>();
+            // Services
+            // services.AddSingleton<IMarkdownParser, MarkdownParser>();
+            // services.AddSingleton<IHtmlRenderer, HtmlRenderer>();
+            // services.AddSingleton<IPdfBuilder, PdfBuilder>();
 
-// Pipeline steps
+            // Pipeline steps
+            // services.AddSingleton<LoadFilesStep>();
+            // services.AddSingleton<SortFilesStep>();
+            // services.AddSingleton<ExtractHeadingsStep>();
+            // services.AddSingleton<BuildTocStep>();
+            // services.AddSingleton<ConvertMarkdownStep>();
+            // services.AddSingleton<BuildPdfStep>();
 
-// Pipeline
+            // Pipelines
+            services.AddSingleton<BuildPipeline>();
 
-// Commands
-services.AddSingleton<BuildCommand>();
+            // Commands
+            services.AddSingleton<BuildPdf>();
 
-var provider = services.BuildServiceProvider();
+            // Build the service provider
+            var provider = services.BuildServiceProvider();
 
-// CLI root
-var root = new RootCommand("Markdown -> PDF builder");
-root.Subcommands.Add(provider.GetRequiredService<BuildCommand>());
+            // CLI root
+            var root = new RootCommand("Creates an e-book from multiple markdown files");
 
-return root.Parse(args).Invoke();
+            //// Supress error message when run without command
+            //root.SetAction((ParseResult parseResult) =>
+            //{
+            //    new HelpAction().Invoke(parseResult);
+            //    return 0;
+            //});
+
+            // Add shared arguments and options
+            var inputOpt = new Argument<string>("--input")
+            {
+                Description = "Input folder",
+                DefaultValueFactory = parseResult => "."
+            };
+            var outputOpt = new Option<string>("--output", "-o")
+            {
+                Description = "Output file name",
+                Required = true
+            };
+            var titleOpt = new Option<string>("--title")
+            {
+                DefaultValueFactory = parseResult => "Document",
+                Description = "Title for the book"
+            };
+            var levelOpt = new Option<int>("--toclevel")
+            {
+                DefaultValueFactory = parseResult => 2,
+                Description = "Heading level for TOC"
+            };
+
+            root.Arguments.Add(inputOpt);
+            root.Options.Add(outputOpt);
+            root.Options.Add(titleOpt);
+            root.Options.Add(levelOpt);
+
+            // Add subcommands
+            root.Subcommands.Add(provider.GetRequiredService<BuildPdf>());
+
+            return root.Parse(args).Invoke();
+        }
+    }
+}
